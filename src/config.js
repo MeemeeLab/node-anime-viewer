e91bc120095dba2a09c13d124a0fa3c6a9cbdd05
MeemeeLab <57629983+MeemeeLab@users.noreply.github.com>
2021-12-27 08:17:29 +0900

    Revert "Fix bug with windows"
    This reverts commit b20f0ff028924719c77ab0f1194030d3d51cd1bf.

--- src/config.js
+++ src/config.js
@@ -1,10 +1,7 @@
 import {readFileSync} from "fs";
 import {resolve} from "path";
 
-let __dirname = new URL('', import.meta.url).pathname;
-if (process.platform === 'win32') {
-  __dirname = __dirname.slice(1); // On Windows, the path starts with a unneeded slash
-}
+const __dirname = new URL('', import.meta.url).pathname;
 const config = JSON.parse(readFileSync(
   resolve(__dirname, "../../package.json"),
 ));
