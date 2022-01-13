2709612bc4ece5cc8021443dec852137e4270ddf
MeemeeLab <57629983+MeemeeLab@users.noreply.github.com>
2022-01-14 00:05:36 +0900

    Updated proxy
    Fixes #41. Don't know why it was working before.

--- src/modules/proxy.js
+++ src/modules/proxy.js
@@ -1,13 +1,21 @@
 import http from 'http';
 import fetch from 'node-fetch';
+import https from 'https';
+
+const ignoreCertificateAgent = new https.Agent({
+    rejectUnauthorized: false
+});
 
 export class Proxy {
     server;
+    destinationHost;
     destination;
     headerOverrides;
     constructor(destination, headerOverrides) {
         this.destination = destination;
         this.headerOverrides = headerOverrides;
+        this.destinationHost = new URL(destination).host;
+        this.headerOverrides['host'] = this.destinationHost;
         this.server = http.createServer(this._handleRequest.bind(this));
         this.server.listen(0);
     }
@@ -16,7 +24,7 @@ export class Proxy {
      * @param {http.ServerResponse} res 
      */
     _handleRequest(req, res) {
-        fetch(this.destination, Object.assign(req.headers, this.headerOverrides))
+        fetch(this.destination, {headers: Object.assign(req.headers, this.headerOverrides), agent: ignoreCertificateAgent})
             .then(resp => {
                 res.writeHead(resp.status, resp.headers.raw());
                 resp.body.pipe(res);
